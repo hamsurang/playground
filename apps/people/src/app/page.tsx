229134678e39d78d8af7b3d397a2da5b2691f22f
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-03-17 20:07:09 +0900

    Update apps/people/src/app/page.tsx
    Co-authored-by: Chung-il Jung <jungchungil7@gmail.com>

--- apps/people/src/app/page.tsx
+++ apps/people/src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
                 ))}
               </Stack.Horizontal>
             </Reorder.Group>
-          </Suspense>{' '}
+          </Suspense>
         </Suspense>
       </Flex.Center>
     </Stack>
